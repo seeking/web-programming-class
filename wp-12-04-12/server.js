b2461738f6ef5e25edc9f0b9a5db6babcd8276c8
George Dyer <georgedyer+gh@me.com>
2012-04-19 16:10:41 -0400

    the wall

--- wp-12-04-12/server.js
+++ wp-12-04-12/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var _ = require('./public/underscore');
 
 var wall = [{
   name:'Wall Master',
@@ -8,20 +9,38 @@ var wall = [{
 }];
 
 var server = http.createServer(function(req,res) {
+  
   var urlObj = url.parse(req.url,true);
 
+  var pathArr = urlObj.pathname.split('/');
+
   if (urlObj.pathname == '/') {
-    console.log('yes');
-    fs.readFile('wall.html','utf8',function(err,theFile) {
+    fs.readFile(__dirname + '/public/wall.html','utf8', function(err,theFile) {
+      if (err) res.end(JSON.stringify(err))
+      else res.end(theFile);
+    });
+  }
+
+  if (pathArr[1] == 'public') {
+    fs.readFile(__dirname + urlObj.pathname,'utf8',function(err, theFile) {
       res.end(theFile);
     });
   }
+
 
   if (urlObj.pathname == '/wall') {
     if (urlObj.query.name) {
       wall.push(urlObj.query);
     }
-    res.end(JSON.stringify(wall));
+    if (urlObj.query.from) {
+      var recentWall = _.filter(wall, function(entry) {
+        return entry.dt > urlObj.query.from
+      });
+      res.end(JSON.stringify(recentWall));
+    } else {
+      res.end(JSON.stringify(wall));
+    }
+    
   }
 
   
