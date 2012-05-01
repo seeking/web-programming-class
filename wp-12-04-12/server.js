572e7be710b56b6fe06a57a70b1b66b98651b221
George Dyer <georgedyer@me.com>
2012-05-01 00:53:43 +0000

    12-04-26

--- wp-12-04-12/server.js
+++ wp-12-04-12/server.js
@@ -14,6 +14,8 @@ http.createServer(function(req,res) {
 
   var pathArr = urlObj.pathname.split('/');
 
+  // if path of request is the root, just pass it the wall.html file
+  
   if (urlObj.pathname == '/') {
     fs.readFile(__dirname + '/public/wall.html','utf8', function(err,theFile) {
       if (err) { 
@@ -21,6 +23,8 @@ http.createServer(function(req,res) {
       } else res.end(theFile);
     });
   }
+  
+  // if the path starts with the public directory, pass any file inside it that is requested
 
   if (pathArr[1] == 'public') {
     fs.readFile(__dirname + urlObj.pathname,'utf8',function(err, theFile) {
@@ -30,6 +34,10 @@ http.createServer(function(req,res) {
     });
   }
 
+  // if the pathname is simply '/wall', assume it is an API call
+  // add comments to the call if requested
+  // if a from time is indicated, send back only comments since that time
+  // otherwise, send back the entire wall
 
   if (urlObj.pathname == '/wall') {
     if (urlObj.query.name) {
